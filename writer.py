072a1b8480d9c013258c436f372ee63bcc51b597
W01fw00d <gabrm_91@hotmail.com>
2018-03-26 19:34:37 +0200

    Implement: word count

--- writer.py
+++ writer.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
 class Writer:
-	def __init__(self, writer, chapter_number, title):
+	def __init__(self, author, chapter_number, title):
 		type_of_file = '.txt'
-	
-		self.line_limit = 80
-		self.text = ''
+		self.line_limit = 90
 		self.file_name = 'chapter_' + str(chapter_number) + type_of_file
+
+		self.text = ''
+		self.word_count = len(title.split())
+
 		self.text_file = open(self.file_name, 'w')
-		self.head(chapter_number, title, writer)
+		self.head(chapter_number, title, author)
 		self.footer = '[Made with Love and https://github.com/W01fw00d/chapter_writer.git]'
 		
 	def head(self, chapter_number, title, writer):
@@ -18,19 +20,32 @@ class Writer:
 		self.text += '\n'
 		self.text += '\n'
 		
-	def write(self, new_text):
-		new_text += '.'
-		lines = [new_text[i:i+self.line_limit] for i in range(0, len(new_text), self.line_limit)]
-		for line in lines:
-			self.text += line + '\n'
+	def writeParagraph(self, input_text):
+		#input_text += '.'
+		splitted_text = input_text.split()
+		next_text = ''
+		current_line = ''
+
+		for word in splitted_text:
+			if (len(current_line + word) <= self.line_limit):
+				current_line += word + ' '
+				
+			else:
+				next_text += current_line + '\n'
+				current_line = word + ' '
 			
-		self.text += '\n'
+			self.word_count += 1
+			
+		next_text += current_line + '\n'
+			
+		self.text += next_text + '\n'
 		
 	def narrate(self, new_text):
-		self.write(new_text)
+		self.writeParagraph(new_text)
 		
 	def end(self):
 		self.text += self.footer
 		self.text_file.write(self.text)
 		self.text_file.close()
 		print(self.file_name + ' succesfully created')
+		print('Words: ' + str(self.word_count))
